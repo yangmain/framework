516ca745258fa64ae8d84108806c4b20cde0d0a9
devops4j <devops4j@devops4j.com>
2018-05-21 09:04:38 +0800

    修改

---
+++ framework-cache/framework-cache-boot-starter/src/main/java/com/rnkrsoft/framework/cache/spring/autoconfig/CacheRegister.java
@@ -0,0 +1,7 @@
+package com.rnkrsoft.framework.cache.spring.autoconfig;
+
+/**
+ * Created by rnkrsoft.com on 2018/5/21.
+ */
+public class CacheRegister {
+}
---
+++ framework-cache/framework-cache-boot-starter/src/main/java/com/rnkrsoft/framework/cache/spring/autoconfig/annotation/EnableCache.java
@@ -0,0 +1,8 @@
+package com.rnkrsoft.framework.cache.spring.autoconfig.annotation;
+
+/**
+ * Created by rnkrsoft.com on 2018/5/21.
+ */
+public @interface EnableCache {
+
+}
