1f0e7a6e5531789558cd008623660eee8f18568d
devops4j <devops4j@devops4j.com>
2018-12-09 22:39:56 +0800

    修改

--- framework-messagequeue/framework-messagequeue-rabbitmq/src/test/java/com/rnkrsoft/framework/messagequeue/rabbitmq/MessageQueueProducerRabbitMQTest.java
+++ framework-messagequeue/framework-messagequeue-rabbitmq/src/test/java/com/rnkrsoft/framework/messagequeue/rabbitmq/MessageQueueProducerRabbitMQTest.java
@@ -1,14 +1,19 @@
 package com.rnkrsoft.framework.messagequeue.rabbitmq;
 
 import com.rnkrsoft.framework.messagequeue.protocol.Message;
+import lombok.Data;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * Created by woate on 2018/12/8.
  */
 public class MessageQueueProducerRabbitMQTest {
+
+    @Data
+    static class Bean1 {
+        String name;
+        int age;
+    }
 
     @Test
     public void testProduce() throws Exception {
@@ -16,7 +21,10 @@ public class MessageQueueProducerRabbitMQTest {
         messageQueueProducerRabbitMQ.setUri("amqp://zxevpop:pro_123456@192.168.100.245:5672");
         messageQueueProducerRabbitMQ.setExchangeName("framework.test");
         messageQueueProducerRabbitMQ.init();
-        Message message = new Message("xxxxxxxxxxxxxxxx");
+        Bean1 bean1 = new Bean1();
+        bean1.age = 2;
+        bean1.setName("xsxsxsx");
+        Message message = new Message(bean1);
         message.setRoutingKey("test.routingkey");
         for (int i = 0; i < 10000; i++) {
             messageQueueProducerRabbitMQ.produce(message);
