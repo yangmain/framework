46113f9ba121341eac0e5b9307c6aecff4964671
devops4j <devops4j@devops4j.com>
2018-12-23 22:45:46 +0800

    修改

--- framework-orm/framework-orm-api/src/main/java/com/rnkrsoft/framework/orm/jdbc/BaseEntity.java
+++ framework-orm/framework-orm-api/src/main/java/com/rnkrsoft/framework/orm/jdbc/BaseEntity.java
@@ -5,11 +5,13 @@ package com.rnkrsoft.framework.orm.jdbc;
  */
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.util.Date;
 
 @Getter
+@Setter
 @ToString(callSuper = true)
 public abstract class BaseEntity extends OrderByEntity {
     @Comment("创建日期")
