145354e260453fceaf9b4a996570a373625b5dcd
devops4j <devops4j@devops4j.com>
2019-01-13 15:52:26 +0800

    1.0.0正式版本

--- framework-sequence/framework-sequence-myisam/src/test/java/com/rnkrsoft/framework/test/sequence/MyISAMSequenceServiceTest.java
+++ framework-sequence/framework-sequence-myisam/src/test/java/com/rnkrsoft/framework/test/sequence/MyISAMSequenceServiceTest.java
@@ -2,20 +2,9 @@ package com.rnkrsoft.framework.test.sequence;
 
 import com.rnkrsoft.framework.orm.jdbc.mysql.DataEngineType;
 import com.rnkrsoft.framework.orm.untils.SqlScriptUtils;
-import com.rnkrsoft.utils.DateUtils;
 import com.rnkrsoft.framework.orm.jdbc.NameMode;
 import com.rnkrsoft.framework.orm.jdbc.WordMode;
-import com.rnkrsoft.framework.sequence.SequenceService;
-import com.rnkrsoft.framework.sequence.SequenceServiceFactory;
-import com.rnkrsoft.framework.sequence.myisam.MyISAMSequenceService;
-import com.rnkrsoft.framework.sequence.myisam.entity.SequenceEntity;
-import com.rnkrsoft.framework.test.CreateTable;
-import com.rnkrsoft.framework.test.DataSource;
-import com.rnkrsoft.framework.test.DataSourceTest;
-import com.rnkrsoft.framework.test.DataSourceType;
-import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.test.context.ContextConfiguration;
 
 import java.io.IOException;
 
